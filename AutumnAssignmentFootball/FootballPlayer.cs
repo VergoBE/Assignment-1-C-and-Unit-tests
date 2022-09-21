222d306cc85ee20f5d08c09c97e70508c017ca15
fight <fight@EMIL_VERGOBE>
2022-09-21 09:26:02 +0200

    Add project files.

---
+++ AutumnAssignmentFootball/FootballPlayer.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutumnAssignmentFootball
+{
+    public class FootballPlayer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public int ShirtNumber { get; set; }
+
+        public FootballPlayer()
+        {
+
+        }
+
+        public FootballPlayer(int id, string name, int age, int shirtNumber)
+        {
+            Id = id;
+            Name = name;
+            Age = age;
+            ShirtNumber = shirtNumber;
+        }
+
+        public override string ToString()
+        {
+            return Id + " " + Name + " " + Age + " " + ShirtNumber;
+        }
+
+        public void AgeValidation()
+        {
+            if (Age < 1) throw new ArgumentOutOfRangeException("Player cannot be younger than 1" + Age);
+        }
+
+        public void NameValidator()
+        {
+            if (Name == null) throw new ArgumentNullException("Name cannot be empty or null" + Name);
+            if (Name.Length < 2) throw new ArgumentOutOfRangeException("Name cannot be less than 2 characters" + Name);
+     
+        }
+
+        public void ShirtNumberValidation()
+        {
+            if (ShirtNumber > 1) throw new ArgumentOutOfRangeException("Shirtnumber cannot exceed 99" + ShirtNumber);
+            if (ShirtNumber < 99) throw new ArgumentOutOfRangeException("Shirtnumber cannot be lower than 1" + ShirtNumber);
+
+        }
+    }
+}
---
+++ AutumnAssignmentFootballTests/FootballPlayerTests.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AutumnAssignmentFootball;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutumnAssignmentFootball.Tests
+{
+    [TestClass()]
+    public class FootballPlayerTests
+    {
+        [TestMethod()]
+        public void AgeValidationTest()
+        {
+            FootballPlayer agevalidation = new FootballPlayer(1, "Jensen", 1, 20);
+            FootballPlayer invalidagevalidation = new FootballPlayer(2, "hansen", 0, 20);
+
+
+            Assert.ThrowsException<ArgumentOutOfRangeException>(invalidagevalidation.AgeValidation);
+        }
+
+        [TestMethod()]
+        public void NameValidatorTest()
+        {
+            FootballPlayer namevalid = new FootballPlayer(1, "Jensen", 1, 20);
+            FootballPlayer nameinvalid = new FootballPlayer(1, null, 1, 20);
+
+            Assert.ThrowsException<ArgumentNullException>(nameinvalid.NameValidator);
+
+        }
+
+        [TestMethod()]
+        public void ShirtNumberValidationTest()
+        {
+            FootballPlayer shirtnumbervalid = new FootballPlayer(1, "Jensen", 1, 99);
+            FootballPlayer shirtnumberinvalid100 = new FootballPlayer(1, "Jensen", 1, 100);
+            FootballPlayer shirtnumberinvalid0 = new FootballPlayer(1, "Jensen", 1, 0);
+
+            Assert.ThrowsException<ArgumentOutOfRangeException>(shirtnumberinvalid100.ShirtNumberValidation);
+            Assert.ThrowsException<ArgumentOutOfRangeException>(shirtnumberinvalid0.ShirtNumberValidation);
+        }
+    }
+}
